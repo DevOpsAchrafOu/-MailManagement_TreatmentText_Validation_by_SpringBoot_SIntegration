d0eccef0dfa53ffb23f9ef9313e81534bc8d8d35
achrafOuakka <ouakka.achraf@gmail.com>
2021-03-06 17:38:37 +0100

    create project

---
+++ src/main/java/com/api/mail/manag/MailManagementTreatmentTextValidationBySpringBootSIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.api.mail.manag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailManagementTreatmentTextValidationBySpringBootSIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailManagementTreatmentTextValidationBySpringBootSIntegrationApplication.class, args);
+	}
+
+}
