a60186b5984075d192175e25a723a4bfd72a3f68
achrafOuakka <ouakka.achraf@gmail.com>
2021-03-06 18:23:12 +0000

    More update

---
+++ src/main/java/com/api/mail/manag/web/InfoRestController.java
@@ -0,0 +1,12 @@
+package com.api.mail.manag.web;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/infos")
+@CrossOrigin("*")
+public class InfoRestController {
+
+}
